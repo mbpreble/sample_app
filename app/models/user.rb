08a3dd9553669c4df6712b302d98d94860a8a4da
Matthew Preble <mbpreble@gmail.com>
2013-04-28 15:53:41 -0500

    Finish chapter 7 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   
   validates :name, presence: true, length: { maximum: 50}
   
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, length: {minimum: 6}
   
   validates :password_confirmation, presence: true
   
